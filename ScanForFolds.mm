57f60f8bc1d75295d6af1ab31f2684520f8d570d
James Walker <jw@jwwalker.com>
2018-11-11 16:52:52 -0800

    Add zlib/libpng copyright.  Change product name to VRML+3DMF.

--- ScanForFolds.mm
+++ ScanForFolds.mm
@@ -6,16 +6,35 @@
  *  Copyright 2006 James W. Walker. All rights reserved.
  *
  */
+/*
+	Copyright (c) 2018 James W. Walker
 
-#include "ScanForFolds.h"
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-#import <Cocoa/Cocoa.h>
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 
-#include "BBLMTextIterator.h"
-#include "FindLanguageType.h"
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
 
-#include <vector>
-#include <cstdio>
+#import "ScanForFolds.h"
+
+#import "BBLMTextIterator.h"
+#import "FindLanguageType.h"
+
+#import <vector>
+#import <cstdio>
 
 enum EState
 {
